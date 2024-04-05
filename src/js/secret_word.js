078aef8ee6d11fae995d0d72afbf1c416645c038
Blondy <blondymartinezm@gmail.com>
2024-04-05 22:38:17 +0200

    added utility functions

--- src/js/secret_word.js
+++ src/js/secret_word.js
@@ -13,12 +13,22 @@ class SecretWord {
         const words = wordsData.split('\n');
         
         this.words = words.filter(word => word.length >= 5 && word.length <= 12);
-        this.randomWord();
+        this.randomWord = this.getRandomWord();
     }
 
-    randomWord() {
+    getRandomWord() {
         const randomIndex = Math.floor(Math.random() * this.words.length);
         const randomWord = this.words[randomIndex];
         return randomWord;
+    }
+
+    // utility
+
+    containsLetter(letter) {
+        return this.randomWord.includes(letter);
+    }
+
+    is(word) {
+        return word == this.randomWord;
     }
 }
