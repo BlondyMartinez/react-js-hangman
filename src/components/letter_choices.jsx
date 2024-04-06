703791ba1118b3bd9556e6778da82801fdb68c45
BlondyMartinez <blondymartinezm@gmail.com>
2024-04-06 20:13:15 +0200

    now shows used letters and styled them

--- src/components/letter_choices.jsx
+++ src/components/letter_choices.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import WrongImg from "../assets/wrong.png"
+import CorrectImg from "../assets/correct.png"
 
 const LetterChoices = (props) => {
+    const [imgSRC, setImgSRC] = useState({});
 
     function handleClick(letter) {
         if (props.word.containsLetter(letter)) {
             props.setCorrectLetters([...props.correctLetters, letter]);
+            setImgSRC((prevSrc) => ({ ...prevSrc, [letter]: CorrectImg }));
+        } else {
+            setImgSRC((prevSrc) => ({ ...prevSrc, [letter]: WrongImg }));
         }
     }
 
@@ -19,9 +25,15 @@ const LetterChoices = (props) => {
             {LETTERS.map((row, rowIndex) => (
                 <div key={rowIndex} className="w-100 d-flex justify-content-around">
                     {row.map((letter, index) => (
-                        <button key={index} onClick={() => handleClick(letter)}>
-                        {letter.toUpperCase()}
-                        </button>
+                        <div key={index} className="position-relative">
+                            <button
+                                onClick={() => handleClick(letter)}
+                                style={{ position: "relative", zIndex: 0 }}
+                            >
+                            {letter.toUpperCase()}
+                            </button>
+                            {imgSRC[letter] && <img className="position-absolute top-50 start-50 translate-middle" src={imgSRC[letter]} />} 
+                        </div>
                     ))}
                 </div>
             ))}
