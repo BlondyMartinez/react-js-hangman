2e66b3aa1e8953bad78f207301f12c97054b27ee
Blondy <blondymartinezm@gmail.com>
2024-04-07 03:30:19 +0200

    separated ttitle into its own component

---
+++ src/components/title.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Title = () => {
+    return (
+        <h1>
+            <span>H</span>
+            <span>A</span>
+            <span>N</span>
+            <span>G</span>
+            <span>M</span>
+            <span>A</span>
+            <span>N</span>
+        </h1>
+    );
+}
+
+export default Title;
