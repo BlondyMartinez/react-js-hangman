1fbbf5d06500e80ed29e005f4def26cfa3d993b1
BlondyMartinez <blondymartinezm@gmail.com>
2024-04-07 06:29:18 +0200

    added game-related logic, restart button and confetti

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
+import ConfettiExplosion from 'react-confetti-explosion';
 import LetterChoices from "./letter_choices";
 import Word from "./word"
 import Title from "./title"
 import SecretWord from "../js/secret_word.js"
+import RestartIMG from "../assets/restart.png"
 import Stage_0 from "../assets/stages/stage_0.png"
 import Stage_1 from "../assets/stages/stage_1.png"
 import Stage_2 from "../assets/stages/stage_2.png"
@@ -17,8 +19,8 @@ function App() {
   const [letterFeedback, setLetterFeedback] = useState({});
   const [win, setWin] = useState(false);
   const [loss, setLoss] = useState(false);
-
   const [stage, setStage] = useState(0);
+
   const STAGE = {
     0: Stage_0,
     1: Stage_1,
@@ -29,8 +31,28 @@ function App() {
     6: Stage_6
   }
 
+  function checkForWin() {
+    if (word && word.isGuessCorrect(word.display(correctLetters))) setWin(true);
+  }
+
+  function checkForLoss() {
+    if (stage == 6) setLoss(true);
+  }
+
+  useEffect(() => {
+    initializeWord();
+  }, []);
+
+  useEffect(() => {
+      checkForLoss();
+  }, [stage]); 
+
+  useEffect(() => {
+      checkForWin();
+  }, [correctLetters]); 
+
   function handleRestart(){
-    setWord(new SecretWord());
+    initializeWord();
     setStage(0);
     setCorrectLetters([]);
     setLetterFeedback({});
@@ -38,20 +60,16 @@ function App() {
     setLoss(false);
   }
 
-
-  useEffect(() => {
-    const initializeWord = async () => {
-      const secretWord = new SecretWord();
-      await secretWord.initialize();
-      setWord(secretWord);
-    };
-
-    initializeWord();
-  }, []);
+  const initializeWord = async () => {
+    const secretWord = new SecretWord();
+    await secretWord.initialize();
+    setWord(secretWord);
+  };
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center mt-5">
       <Title />
+      {win && <ConfettiExplosion force={0.8} duration={3000} particleCount={250} width={1600} />}
       <div className="d-flex" style={{ width: "80vw" }}>
         <img className="stage" src={STAGE[stage]} />
         <div className="d-flex flex-column gap-3 justify-content-around align-items-center w-100">
@@ -65,10 +83,9 @@ function App() {
             stage={stage} 
             setStage={setStage} 
             win={win}
-            setWin={setWin}
             loss={loss}
-            setLoss={setLoss}
           />}
+          <button onClick={handleRestart} className="btn restart"><img src={RestartIMG} /></button>
         </div>
       </div>
       <a href="https://www.linkedin.com/in/blondy-martinez/" target="_blank" className="fixed-bottom p-4 text-center footer">By Blondy Martinez</a>
